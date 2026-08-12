2e6f51d14470c51bad0177e44c621ff65b5ac589
MaloviniV <malovinivjd@hotmail.com>
2026-08-12 11:50:43 -0300

    creacion del proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PROY_INMOBILIARIA_malovini.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
